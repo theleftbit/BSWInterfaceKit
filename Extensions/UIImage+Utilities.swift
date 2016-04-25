766ccd37bc9039797b71e6cf915b227f4d68b913
Pierluigi Cifani <piercifani@gmail.com>
2016-04-25 11:39:05 +0200

    OnboardingViewController works (it's ugly, but for now we can live with it)

--- Extensions/UIImage+Utilities.swift
+++ Extensions/UIImage+Utilities.swift
@@ -10,15 +10,8 @@ extension UIImage {
     public class func interfaceKitImageNamed(name: String, compatibleWithTraitCollection: UITraitCollection? = nil) -> UIImage? {
         return UIImage(
             named: name,
-            inBundle: InterfaceBundle.bundle,
+            inBundle: NSBundle.interfaceKitBundle(),
             compatibleWithTraitCollection: compatibleWithTraitCollection
         )
     }
-}
-
-
-//Due to some Cocoapods limitations, there is no way of adding resources directly to a framework. This sucks.
-private struct InterfaceBundle {
-    private static let bundle = NSBundle(forClass: InterfaceKit.self)
-    private class InterfaceKit: NSObject {}
 }
--- ViewControllers/OnboardingViewController.swift
+++ ViewControllers/OnboardingViewController.swift
@@ -28,12 +28,106 @@ class LaunchScreenViewController: UIViewController {
 
 class OnboardingViewController: UIViewController {
 
-    weak var onboardingObserver: OnboardingObserver?
-    weak var onboardingDataSource: OnboardingDataSource?
+    weak var onboardingObserver: OnboardingObserver!
+    weak var onboardingDataSource: OnboardingDataSource!
+    static let Spacing: CGFloat = 10
+    
+    let socialStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .Vertical
+        stackView.alignment = .Fill
+        stackView.distribution = .FillProportionally
+        stackView.spacing = OnboardingViewController.Spacing
+        return stackView
+    }()
+
+    let logoStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .Vertical
+        stackView.alignment = .Fill
+        stackView.distribution = .FillProportionally
+        stackView.spacing = OnboardingViewController.Spacing
+        return stackView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.blueColor()
+        
+        //First, the background
+        switch onboardingDataSource.onboardingBackground() {
+        case .Color(let color):
+            view.backgroundColor = color
+        case .Image(let image):
+            let imageView = UIImageView(image: image)
+            view.addSubview(imageView)
+            constrain(imageView) { imageView in
+                imageView.edges == imageView.superview!.edges
+            }
+        }
+        
+        //Then, the stackViews
+        view.addSubview(logoStackView)
+        view.addSubview(socialStackView)
+        constrain(logoStackView, socialStackView) { logoStackView, socialStackView in
+            logoStackView.top == logoStackView.superview!.top + OnboardingViewController.Spacing
+            logoStackView.leading == logoStackView.superview!.leading + OnboardingViewController.Spacing
+            logoStackView.trailing == logoStackView.superview!.trailing - OnboardingViewController.Spacing
+
+            socialStackView.bottom == socialStackView.superview!.bottom - OnboardingViewController.Spacing
+            socialStackView.leading == socialStackView.superview!.leading + OnboardingViewController.Spacing
+            socialStackView.trailing == socialStackView.superview!.trailing - OnboardingViewController.Spacing
+        }
+        
+        prepareLogoStackView()
+        prepareSocialStackView()
     }
     
+    @objc func onLoginFacebook() {
+        onboardingObserver.onFacebookAuthenticationRequested()
+    }
+    
+    //MARK:- Private
+
+    private func prepareSocialStackView() {
+        
+        let privacyLabel: UILabel = {
+            let label = UILabel()
+            label.text = "We will never post in your wall"
+            label.textAlignment = .Center
+            label.numberOfLines = 0
+            return label
+        }()
+        
+        socialStackView.addArrangedSubview(privacyLabel)
+        
+        let facebookButton: UIButton = {
+            let button = UIButton()
+            button.backgroundColor = UIColor.blueColor()
+            button.setTitle("Login with Facebook", forState: .Normal)
+            button.addTarget(self, action: #selector(onLoginFacebook), forControlEvents: UIControlEvents.TouchDown)
+            return button
+        }()
+        
+        socialStackView.addArrangedSubview(facebookButton)
+    }
+    
+    private func prepareLogoStackView() {
+        
+        let logoView: UIImageView = {
+            let imageView = UIImageView(image: onboardingDataSource.onboardingAppLogo())
+            imageView.contentMode = .ScaleAspectFit
+            return imageView
+        }()
+        
+        logoStackView.addArrangedSubview(logoView)
+        
+        let sloganLabel: UILabel = {
+            let label = UILabel()
+            label.textAlignment = .Center
+            label.attributedText = onboardingDataSource.onboardingAppSlogan()
+            return label
+        }()
+        
+        logoStackView.addArrangedSubview(sloganLabel)
+    }
 }
