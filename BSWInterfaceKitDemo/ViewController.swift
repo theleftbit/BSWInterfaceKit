85025d816d3a1a6ab8f5d129a9344686f848a881
Pierluigi Cifani <piercifani@gmail.com>
2016-04-30 17:16:07 +0200

    Created demo project

---
+++ BSWInterfaceKitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BSWInterfaceKitDemo
+//
+//  Created by Pierluigi Cifani on 30/04/16.
+//  Copyright © 2016 Blurred Software SL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
