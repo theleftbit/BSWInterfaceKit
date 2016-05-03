7506728b9687173342fdcce6df951c34e9d038c4
Pierluigi Cifani <piercifani@gmail.com>
2016-05-03 12:24:47 +0200

    Create BSWSplitViewController

---
+++ Source/ViewControllers/BSWSplitViewController.swift
@@ -0,0 +1,44 @@
+//
+//  Created by Pierluigi Cifani on 03/05/16.
+//  Copyright © 2016 Blurred Software SL. All rights reserved.
+//
+
+import UIKit
+
+public class BSWSplitViewController<Master: UIViewController, Detail: UIViewController>: UISplitViewController, UISplitViewControllerDelegate {
+    
+    public weak var masterVC: Master!
+    public weak var detailVC: Detail!
+    
+    public init(masterVC: Master, detailVC: Detail) {
+        
+        self.masterVC = masterVC
+        self.detailVC = detailVC
+        
+        super.init(nibName: nil, bundle: nil)
+        viewControllers = [
+            UINavigationController(rootViewController: masterVC),
+            UINavigationController(rootViewController: detailVC),
+        ]
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.delegate = self
+    }
+    
+    //MARK:- UISplitViewControllerDelegate
+    
+    public func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController: UIViewController, ontoPrimaryViewController primaryViewController: UIViewController) -> Bool {
+        
+        //TODO: Review on iPad
+        
+        return true
+    }
+    
+}
