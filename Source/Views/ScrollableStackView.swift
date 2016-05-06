159074b660106fb297a9dabc9888db0e991dc8fb
Pierluigi Cifani <piercifani@gmail.com>
2016-05-06 16:52:25 +0200

    PhotoGalleryView seems to work OK

--- Source/Views/ScrollableStackView.swift
+++ Source/Views/ScrollableStackView.swift
@@ -7,30 +7,35 @@ import Foundation
 
 public class ScrollableStackView: UIView {
     
-    public let scrollView: UIScrollView = {
-        let scrollView = UIScrollView()
-        return scrollView
-    }()
+    public let scrollView = UIScrollView()
+    public let stackView = UIStackView()
     
-    public let stackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .Vertical
-        return stackView
-    }()
- 
-    public init() {
+    public init(axis: UILayoutConstraintAxis = .Vertical,
+                alignment: UIStackViewAlignment = .Fill) {
         super.init(frame: CGRectZero)
+        
+        stackView.axis = axis
+        stackView.alignment = alignment
+
         addSubview(scrollView)
         scrollView.addSubview(stackView)
 
         scrollView.fillSuperview()
         stackView.fillSuperview()
+        
+        switch axis {
+        case .Horizontal:
+            stackView.heightAnchor.constraintEqualToAnchor(scrollView.heightAnchor).active = true
+        case .Vertical:
+            stackView.widthAnchor.constraintEqualToAnchor(scrollView.widthAnchor).active = true
+        }
+        
+        clipsToBounds = true
     }
-    
+
     required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
     
     public override class func requiresConstraintBasedLayout() -> Bool {
         return true
