dfb6d1eeaebfb61e6aa4458420b453aebe199205
Pierluigi Cifani <piercifani@gmail.com>
2016-05-09 12:47:38 +0200

    Move everything to UIView+Utils

--- Source/Extensions/UIView+Utils.swift
+++ Source/Extensions/UIView+Utils.swift
@@ -46,4 +46,13 @@ extension UIView {
             view.edges == inset(view.superview!.edges, edges.top, edges.left, edges.bottom, edges.right)
         }
     }
+    
+    public class func instantiateFromNib<T: UIView>(viewType: T.Type) -> T? {
+        let className = NSStringFromClass(viewType).componentsSeparatedByString(".").last!
+        return NSBundle.mainBundle().loadNibNamed(className, owner: nil, options: nil).first as? T
+    }
+    
+    public class func instantiateFromNib() -> Self? {
+        return instantiateFromNib(self)
+    }
 }
