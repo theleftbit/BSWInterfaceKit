6eddcf726b114b17628a6b8355fda65967b74f54
Pierluigi Cifani <piercifani@gmail.com>
2016-05-13 12:51:26 +0200

    Remove SDWebImage, move to PINRemoteImage

--- Source/Extensions/UIImageView+Download.swift
+++ Source/Extensions/UIImageView+Download.swift
@@ -3,34 +3,39 @@
 //  Copyright © 2016 Blurred Software SL. All rights reserved.
 //
 
-import SDWebImage
+import PINRemoteImage
+import BSWFoundation
 
 public typealias BSWImageDownloaderProgressBlock = (Int, Int) -> Void
-public typealias BSWImageCompletionBlock = (UIImage!, NSError!, SDImageCacheType, NSURL!) -> Void
+public typealias BSWImageCompletionBlock = (Result<UIImage>) -> Void
 
 extension UIImageView {
 
-    private struct BSWImageOptions {
-        private static let Default = SDWebImageOptions()
-    }
-
     public func bsw_setImageFromURLString(url: String) {
         if let url = NSURL(string: url) {
-            sd_setImageWithURL(url)
+            pin_setImageFromURL(url)
         }
     }
 
     public func bsw_cancelImageLoadFromURL() {
-        sd_cancelCurrentImageLoad()
+        pin_cancelImageDownload()
     }
     
-    public func bsw_setImageWithURL(url: NSURL, progress progressBlock: BSWImageDownloaderProgressBlock? = nil, completed completedBlock: BSWImageCompletionBlock? = nil) {
-        sd_setHighlightedImageWithURL(
-            url,
-            options: BSWImageOptions.Default,
-            progress: progressBlock,
-            completed: completedBlock
-        )
+    public func bsw_setImageWithURL(url: NSURL, completed completedBlock: BSWImageCompletionBlock? = nil) {
+        
+        pin_setImageFromURL(url) { (downloadResult) in
+
+            let result: Result<UIImage>
+            if let image = downloadResult.image {
+                result = Result(value: image)
+            } else if let error = downloadResult.error {
+                result = Result(error: error)
+            } else {
+                result = Result(error: NSError(domain: "com.bswinterfacekit.uiimageview", code: 0, userInfo: nil))
+            }
+            
+            completedBlock?(result)
+        }
     }
     
     public func bsw_setPhoto(photo: Photo) {
@@ -39,11 +44,11 @@ extension UIImageView {
             self.image = image
         case .URL(let url):
             backgroundColor = photo.averageColor
-            bsw_setImageWithURL(url, completed: { image, error, _, _ in
-                guard error == nil else { return }
-                self.image = image
+            bsw_setImageWithURL(url) { result in
+                guard result.error == nil else { return }
+                self.image = result.value
                 self.backgroundColor = nil
-            })
+            }
         }
     }
 }
