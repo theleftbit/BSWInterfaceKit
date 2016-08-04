17d2ba456d683add54cc7426802fb94d38613797
Pierluigi Cifani <piercifani@gmail.com>
2016-08-04 11:17:08 +0200

    Create Sample Profiles in BSWInterfaceKit instead of the playground

--- BSWInterfaceKitPlayground/BSWInterfaceKit.playground/Pages/ClassicProfileViewController.xcplaygroundpage/Contents.swift
+++ BSWInterfaceKitPlayground/BSWInterfaceKit.playground/Pages/ClassicProfileViewController.xcplaygroundpage/Contents.swift
@@ -8,19 +8,7 @@ import Deferred
 
 XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
 
-let viewModel = ClassicProfileViewModel(
-    photos: [
-        Photo(kind: Photo.Kind.URL(NSURL(string: "http://ow.ly/ZHvD302NKEF")!))
-    ],
-    titleInfo: TextStyler.styler.attributedString("Gianluigi Buffon", forStyle: .Title),
-    detailsInfo: TextStyler.styler.attributedString("The best keeper of football history", forStyle: .Body),
-    extraInfo: [
-        TextStyler.styler.attributedString("1 World Cup 2006", forStyle: .Body),
-        TextStyler.styler.attributedString("7 Serie A titles", forStyle: .Body),
-        TextStyler.styler.attributedString("3 Coppa Italia", forStyle: .Body),
-        TextStyler.styler.attributedString("1 Uefa Cup", forStyle: .Body)
-    ]
-)
+let viewModel = ClassicProfileViewModel.buffon()
 
 let dataProvider = Future(Deferred(value: Result(viewModel)))
 let detailVC = ClassicProfileViewController(dataProvider: dataProvider)
