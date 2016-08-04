eeb355dd7a7ffb6e8b2a2081338784b8f2703f31
Pierluigi Cifani <piercifani@gmail.com>
2016-08-04 11:17:08 +0200

    Create Sample Profiles in BSWInterfaceKit instead of the playground

--- Source/ViewControllers/Profiles/ClassicProfileViewModel.swift
+++ Source/ViewControllers/Profiles/ClassicProfileViewModel.swift
@@ -29,3 +29,23 @@ public struct ClassicProfileViewModel {
         )
     }
 }
+
+// MARK: - Sample Profiles
+
+extension ClassicProfileViewModel {
+    public static func buffon() -> ClassicProfileViewModel {
+        return ClassicProfileViewModel(
+            photos: [
+                Photo(kind: Photo.Kind.URL(NSURL(string: "http://ow.ly/ZHvD302NKEF")!))
+            ],
+            titleInfo: TextStyler.styler.attributedString("Gianluigi Buffon", forStyle: .Title),
+            detailsInfo: TextStyler.styler.attributedString("The best keeper of football history", forStyle: .Body),
+            extraInfo: [
+                TextStyler.styler.attributedString("1 World Cup 2006", forStyle: .Body),
+                TextStyler.styler.attributedString("7 Serie A titles", forStyle: .Body),
+                TextStyler.styler.attributedString("3 Coppa Italia", forStyle: .Body),
+                TextStyler.styler.attributedString("1 Uefa Cup", forStyle: .Body)
+            ]
+        )
+    }
+}
