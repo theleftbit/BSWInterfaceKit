631956c99dbb09940a2baa186bd31941bff03469
Pierluigi Cifani <piercifani@gmail.com>
2016-08-16 18:13:06 +0200

    Create category of UIWindow

---
+++ Source/Extensions/UIWindow+Utilities.swift
@@ -0,0 +1,26 @@
+//
+//  Created by Pierluigi Cifani on 16/08/16.
+//  Copyright © 2016 Blurred Software SL. All rights reserved.
+//
+
+import UIKit
+
+public extension UIWindow {
+    public var visibleViewController: UIViewController? {
+        return UIWindow.getVisibleViewControllerFrom(self.rootViewController)
+    }
+    
+    public static func getVisibleViewControllerFrom(vc: UIViewController?) -> UIViewController? {
+        if let nc = vc as? UINavigationController {
+            return UIWindow.getVisibleViewControllerFrom(nc.visibleViewController)
+        } else if let tc = vc as? UITabBarController {
+            return UIWindow.getVisibleViewControllerFrom(tc.selectedViewController)
+        } else {
+            if let pvc = vc?.presentedViewController {
+                return UIWindow.getVisibleViewControllerFrom(pvc)
+            } else {
+                return vc
+            }
+        }
+    }
+}
