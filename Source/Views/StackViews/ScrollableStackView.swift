801c53ac264bb1bd4d7fddcb35f1a2235be62d79
Pierluigi Cifani <pcifani@blurredsoftware.com>
2017-02-28 12:23:57 +0100

    Move addArrangedSubview(_ subview: UIView, layoutMargins: UIEdgeInsets) to UIStackView+Utiltiites

--- Source/Views/StackViews/ScrollableStackView.swift
+++ Source/Views/StackViews/ScrollableStackView.swift
@@ -33,33 +33,7 @@ open class ScrollableStackView: UIScrollView {
     }
 
     open func addArrangedSubview(_ subview: UIView, layoutMargins: UIEdgeInsets) {
-
-        subview.translatesAutoresizingMaskIntoConstraints = false
-        
-        let dummyView: UIView = {
-            let view = UIView()
-            view.translatesAutoresizingMaskIntoConstraints = false
-            view.layoutMargins = layoutMargins
-            return view
-        }()
-        
-        dummyView.addSubview(subview)
-
-        subview.topAnchor.constraint(equalTo: dummyView.layoutMarginsGuide.topAnchor).isActive = true
-        subview.bottomAnchor.constraint(equalTo: dummyView.layoutMarginsGuide.bottomAnchor).isActive = true
-
-        //If the subview will be used for text layout, use readableContentGuide instead
-        let layoutGuideX: UILayoutGuide
-        if subview.isKind(of: UILabel.self) || subview.isKind(of: UITextView.self) {
-            layoutGuideX = dummyView.readableContentGuide
-        } else {
-            layoutGuideX = dummyView.layoutMarginsGuide
-        }
-
-        subview.leadingAnchor.constraint(equalTo: layoutGuideX.leadingAnchor).isActive = true
-        subview.trailingAnchor.constraint(equalTo: layoutGuideX.trailingAnchor).isActive = true
-
-        stackView.addArrangedSubview(dummyView)
+        stackView.addArrangedSubview(subview, layoutMargins: layoutMargins)
     }
     
     open func addArrangedSubview(_ subview: UIView) {
