5a079c2660773ce380651d632cfd8fb7630ff4d9
Pierluigi Cifani <pcifani@blurredsoftware.com>
2018-05-18 13:29:55 +0200

    Add wiggle and remove buttons to UICollectionView cells

---
+++ Source/Extensions/UICollectionViewCell+Utilities.swift
@@ -0,0 +1,93 @@
+import UIKit
+import ObjectiveC
+import BSWFoundation
+
+public extension UICollectionViewCell {
+
+    private enum Constants {
+        static let DeleteButtonTag = 876
+        static let Spacing: CGFloat = 7
+    }
+
+    fileprivate struct AssociatedBlockHost {
+        static var imageHost = "imageHost"
+        static var handlerHost = "handlerHost"
+    }
+
+    var editingImage: UIImage? {
+        get {
+            guard let image = objc_getAssociatedObject(self, &AssociatedBlockHost.imageHost) as? UIImage else { return nil }
+            return image
+        } set {
+            objc_setAssociatedObject(self, &AssociatedBlockHost.imageHost, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
+        }
+    }
+
+    var onEdit: VoidHandler? {
+        get {
+            guard let handler = objc_getAssociatedObject(self, &AssociatedBlockHost.handlerHost) as? VoidHandler else { return nil }
+            return handler
+        } set {
+            objc_setAssociatedObject(self, &AssociatedBlockHost.handlerHost, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
+        }
+    }
+
+    var isEditing: Bool {
+        get {
+            return self.viewWithTag(Constants.DeleteButtonTag) != nil
+        } set {
+            if newValue {
+                startWiggling()
+                let buttonImage = self.editingImage ?? UIImage.templateImage(.cancelRound)
+                let removeButton = UIButton(type: .custom)
+                removeButton.addTarget(self, action: #selector(onEditButtonPressed), for: .touchDown)
+                removeButton.setImage(buttonImage, for: .normal)
+                contentView.addAutolayoutSubview(removeButton)
+                NSLayoutConstraint.activate([
+                    removeButton.topAnchor.constraint(equalTo: contentView.topAnchor, constant: Constants.Spacing),
+                    removeButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: Constants.Spacing)
+                    ])
+            } else {
+                stopWiggling()
+                viewWithTag(Constants.DeleteButtonTag)?.removeFromSuperview()
+            }
+        }
+    }
+
+    func startWiggling() {
+        guard contentView.layer.animation(forKey: "wiggle") == nil else { return }
+        guard contentView.layer.animation(forKey: "bounce") == nil else { return }
+
+        let angle = 0.04
+
+        let wiggle = CAKeyframeAnimation(keyPath: "transform.rotation.z")
+        wiggle.values = [-angle, angle]
+
+        wiggle.autoreverses = true
+        wiggle.duration = randomInterval(0.1, variance: 0.005)
+        wiggle.repeatCount = Float.infinity
+
+        contentView.layer.add(wiggle, forKey: "wiggle")
+
+        let bounce = CAKeyframeAnimation(keyPath: "transform.translation.y")
+        bounce.values = [0.5, 0.0]
+
+        bounce.autoreverses = true
+        bounce.duration = randomInterval(0.12, variance: 0.025)
+        bounce.repeatCount = Float.infinity
+
+        contentView.layer.add(bounce, forKey: "bounce")
+    }
+
+    func stopWiggling() {
+        contentView.layer.removeAllAnimations()
+    }
+
+    @objc func onEditButtonPressed() {
+        self.onEdit?()
+    }
+
+    private func randomInterval(_ interval: TimeInterval, variance: Double) -> TimeInterval {
+        return interval + variance * Double((Double(arc4random_uniform(1000)) - 500.0) / 500.0)
+    }
+}
