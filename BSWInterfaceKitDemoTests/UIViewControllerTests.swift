a3c567e6f486bf8ae60473dfb132e27c5e2a43e7
Pierluigi Cifani <pcifani@theleftbit.com>
2018-07-10 10:48:19 +0200

    Add margins to buttom action button

--- BSWInterfaceKitDemoTests/UIViewControllerTests.swift
+++ BSWInterfaceKitDemoTests/UIViewControllerTests.swift
@@ -11,13 +11,32 @@ class UIViewControllerTests: BSWSnapshotTest {
         let vc = BottomActionVC()
         waitABitAndVerify(viewController: vc)
     }
+
+    func testAddBottomActionButtonWithMargin() {
+        let vc = BottomActionVC()
+        vc.margin = UIEdgeInsets(top: 0, left: 20, bottom: 20, right: 20)
+        waitABitAndVerify(viewController: vc)
+    }
 }
 
 private class BottomActionVC: UIViewController {
+    
+    var margin: UIEdgeInsets = .zero
+    var button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         let config = ButtonConfiguration(title: "Send Wink", titleColor: .white, backgroundColor: .red, contentInset: .zero) { }
-        addBottomActionButton(config)
+        button = addBottomActionButton(buttonConfig: config, margin: margin)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        if #available(iOS 11.0, *), let button = self.button {
+            print(button.safeAreaInsets)
+            print(button.intrinsicContentSize)
+
+        }
+        
     }
 }
