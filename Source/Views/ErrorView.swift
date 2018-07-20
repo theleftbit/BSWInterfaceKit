7897a20fbe2f0cd5c8f897ceb89382bcfc1b75a1
Pierluigi Cifani <pcifani@theleftbit.com>
2018-07-20 13:13:38 +0200

    Just subclass UIStackView

--- Source/Views/ErrorView.swift
+++ Source/Views/ErrorView.swift
@@ -5,19 +5,11 @@
 
 import UIKit
 
-open class ErrorView: UIView {
+open class ErrorView: UIStackView {
     
     private enum Constants {
         static let Spacing: CGFloat = 10
     }
-        
-    fileprivate let stackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .vertical
-        stackView.alignment = .center
-        stackView.spacing = Constants.Spacing
-        return stackView
-    }()
     
     public convenience init(listConfig: ActionableListConfiguration) {
         self.init(title: listConfig.title, message: listConfig.message, image: listConfig.image, buttonConfiguration: listConfig.buttonConfiguration)
@@ -25,35 +17,36 @@ open class ErrorView: UIView {
     
     public init(title: NSAttributedString, message: NSAttributedString? = nil, image: UIImage? = nil, buttonConfiguration: ButtonConfiguration? = nil) {
         super.init(frame: .zero)
-        self.addAutolayoutSubview(stackView)
-        stackView.centerInSuperview()
-
+        axis = .vertical
+        alignment = .center
+        spacing = Constants.Spacing
+        
         if let image = image {
             let imageView = UIImageView(image: image)
-            stackView.addArrangedSubview(imageView)
+            addArrangedSubview(imageView)
         }
-
+        
         let label = UILabel()
         label.numberOfLines = 0
         label.textAlignment = .center
         label.attributedText = title
-        stackView.addArrangedSubview(label)
-
+        addArrangedSubview(label)
+        
         if let message = message {
             let label = UILabel()
             label.numberOfLines = 0
             label.textAlignment = .center
             label.attributedText = message
-            stackView.addArrangedSubview(label)
+            addArrangedSubview(label)
         }
-
+        
         if let buttonConfiguration = buttonConfiguration {
             let button = UIButton(buttonConfiguration: buttonConfiguration)
-            stackView.addArrangedSubview(button)
+            addArrangedSubview(button)
         }
     }
     
-    required public init?(coder aDecoder: NSCoder) {
+    required public init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
