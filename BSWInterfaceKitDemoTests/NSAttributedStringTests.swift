93cc55f109ea21c0b98d456ea4e226801de8583a
Pierluigi Cifani <pcifani@theleftbit.com>
2019-05-13 17:21:04 +0200

    Add sparkles of colours

--- BSWInterfaceKitDemoTests/NSAttributedStringTests.swift
+++ BSWInterfaceKitDemoTests/NSAttributedStringTests.swift
@@ -15,4 +15,14 @@ class NSAttributedStringTests: XCTestCase {
         )
         assertSnapshot(matching: attributedString, as: .dump)
     }
+
+    func testLinksColor() {
+        let attributedString = NSMutableAttributedString(string: "Welcome to the jungle")
+        attributedString.addLink(
+            onSubstring: "jungle",
+            linkURL: URL(string: "https://www.youtube.com/watch?v=o1tj2zJ2Wvg")!,
+            linkColor: UIColor.red
+        )
+        assertSnapshot(matching: attributedString, as: .dump)
+    }
 }
