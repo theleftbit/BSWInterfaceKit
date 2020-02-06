09ccf9e453f90f77f9a923e0ad19ff691933e52a
Pierluigi Cifani <pcifani@theleftbit.com>
2020-02-06 21:56:16 +0100

    Add an API to improve layout on SizeClass changes

--- Sources/BSWInterfaceKitObjC/Extensions/UIViewController+Utilities.m
+++ Sources/BSWInterfaceKitObjC/Extensions/UIViewController+Utilities.m
@@ -20,6 +20,9 @@
 
         [self swizzle:@selector(viewWillTransitionToSize:withTransitionCoordinator:)
            withCustom:@selector(bsw_viewWillTransitionToSize:withTransitionCoordinator:)];
+
+        [self swizzle:@selector(willTransitionToTraitCollection:withTransitionCoordinator:)
+           withCustom:@selector(bsw_willTransitionToTraitCollection:withTransitionCoordinator:)];
     });
 }
     
@@ -46,7 +49,34 @@
 
 #pragma mark viewWillTransitionToSize:withTransitionCoordinator:
 
-- (void)bsw_viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id <UIViewControllerTransitionCoordinator>)coordinator {
+- (void)bsw_willTransitionToTraitCollection:(UITraitCollection *)newCollection
+                  withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator {
+    [self bsw_willTransitionToTraitCollection:newCollection withTransitionCoordinator:coordinator];
+    if (self.traitCollection.horizontalSizeClass == newCollection.horizontalSizeClass) {
+        return;
+    }
+    switch (newCollection.horizontalSizeClass) {
+        case UIUserInterfaceSizeClassCompact:
+            {
+                [NSLayoutConstraint deactivateConstraints:[self bsw_regularConstraints]];
+                [NSLayoutConstraint activateConstraints:[self bsw_compactConstraints]];
+            }
+            break;
+        case UIUserInterfaceSizeClassRegular:
+        {
+            [NSLayoutConstraint deactivateConstraints:[self bsw_compactConstraints]];
+            [NSLayoutConstraint activateConstraints:[self bsw_regularConstraints]];
+        }
+            break;
+        default:
+            break;
+    }
+}
+
+#pragma mark viewWillTransitionToSize:withTransitionCoordinator:
+
+- (void)bsw_viewWillTransitionToSize:(CGSize)size
+           withTransitionCoordinator:(id <UIViewControllerTransitionCoordinator>)coordinator {
     [self bsw_viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
     UIView *loadedView = [self viewIfLoaded];
     if (loadedView == nil) {
@@ -84,6 +114,40 @@
     
 - (void)viewInitialLayoutDidComplete {
     // To be overriden by subclasses
+}
+
+#pragma mark Regular / Compact helpers
+
+- (void)setBSWRegularConstraints:(NSArray<NSLayoutConstraint *>*)constraints {
+    objc_setAssociatedObject(self, @selector(bsw_regularConstraints), constraints, OBJC_ASSOCIATION_COPY);
+}
+    
+- (NSArray<NSLayoutConstraint *>*)bsw_regularConstraints {
+    return objc_getAssociatedObject(self, @selector(bsw_regularConstraints));
+}
+
+- (void)setBSWCompactConstraints:(NSArray<NSLayoutConstraint *>*)constraints {
+    objc_setAssociatedObject(self, @selector(bsw_compactConstraints), constraints, OBJC_ASSOCIATION_COPY);
+}
+    
+- (NSArray<NSLayoutConstraint *>*)bsw_compactConstraints {
+    return objc_getAssociatedObject(self, @selector(bsw_compactConstraints));
+}
+
+- (void)addConstraintsForCompactSizeClass:(NSArray<NSLayoutConstraint *>*)compactConstraints
+                         regularSizeClass:(NSArray<NSLayoutConstraint *>*)regularConstraints {
+    [self setBSWCompactConstraints:compactConstraints];
+    [self setBSWRegularConstraints:regularConstraints];
+    switch (self.traitCollection.horizontalSizeClass) {
+        case UIUserInterfaceSizeClassCompact:
+            [NSLayoutConstraint activateConstraints:[self bsw_compactConstraints]];
+            break;
+        case UIUserInterfaceSizeClassRegular:
+            [NSLayoutConstraint activateConstraints:[self bsw_regularConstraints]];
+            break;
+        default:
+            break;
+    }
 }
 
 @end
