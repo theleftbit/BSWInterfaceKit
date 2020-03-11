d1a8e8a1468b3d0b5a45f332b3c48f1828b90273
Michele Restuccia <michele@theleftbit.com>
2020-03-11 12:48:02 +0100

    Customize backgroundColor

--- Tests/BSWInterfaceKitTests/TestSuite/CheckBoxTests.swift
+++ Tests/BSWInterfaceKitTests/TestSuite/CheckBoxTests.swift
@@ -5,7 +5,7 @@ import BSWInterfaceKit
 
 class CheckboxButtonTests: BSWSnapshotTest {
     
-    func testRadioButton() {
+    func testCheckboxButton() {
         let button = CheckboxButton()
         let vm = CheckboxButton.VM(attributedText:
             TextStyler.styler.attributedString(
@@ -14,12 +14,37 @@ class CheckboxButtonTests: BSWSnapshotTest {
         verify(view: button, vm: vm)
     }
     
-    func testRadioButton_Selected() {
+    func testCheckboxButton_Selected() {
         let button = CheckboxButton()
         let vm = CheckboxButton.VM(
-            attributedText: TextStyler.styler.attributedString("You're telling the enemy exactly what you're going to do. No wonder you've been fighting Lorem Ipsum your entire adult life"),
+            attributedText: TextStyler.styler.attributedString("You're telling the enemy exactly what you're going to do. No wonder you've been fighting Lorem Ipsum your entire adult life"
+            ),
             isSelected: true,
             tintColor: .red
+        )
+        verify(view: button, vm: vm)
+    }
+    
+    func testCheckboxButton_WithBackground() {
+        let button = CheckboxButton()
+        let vm = CheckboxButton.VM(
+            attributedText: TextStyler.styler.attributedString(
+                "You're telling the enemy exactly what you're going to do. No wonder you've been fighting Lorem Ipsum your entire adult life"
+            ),
+            backgroundColor: .darkGray
+        )
+        verify(view: button, vm: vm)
+    }
+    
+    func testCheckboxButton_WithBackground_Selected() {
+        let button = CheckboxButton()
+        let vm = CheckboxButton.VM(
+            attributedText: TextStyler.styler.attributedString(
+                "You're telling the enemy exactly what you're going to do. No wonder you've been fighting Lorem Ipsum your entire adult life"
+            ),
+            isSelected: true,
+            tintColor: .red,
+            backgroundColor: .darkGray
         )
         verify(view: button, vm: vm)
     }
