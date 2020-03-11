b5a073b64d5616a06271986b273fffda0c58a932
Michele Restuccia <michele@theleftbit.com>
2020-03-11 12:26:14 +0100

    Change Checkbox for SelectableTableView wrapper

---
+++ Tests/BSWInterfaceKitTests/TestSuite/CheckBoxTests.swift
@@ -0,0 +1,28 @@
+#if canImport(UIKit)
+
+import XCTest
+import BSWInterfaceKit
+
+class CheckboxButtonTests: BSWSnapshotTest {
+    
+    func testRadioButton() {
+        let button = CheckboxButton2()
+        let vm = CheckboxButton2.VM(attributedText:
+            TextStyler.styler.attributedString(
+                "You're telling the enemy exactly what you're going to do. No wonder you've been fighting Lorem Ipsum your entire adult life")
+        )
+        verify(view: button, vm: vm)
+    }
+    
+    func testRadioButton_Selected() {
+        let button = CheckboxButton2()
+        let vm = CheckboxButton2.VM(
+            attributedText: TextStyler.styler.attributedString("You're telling the enemy exactly what you're going to do. No wonder you've been fighting Lorem Ipsum your entire adult life"),
+            isSelected: true,
+            tintColor: .red
+        )
+        verify(view: button, vm: vm)
+    }
+}
+
+#endif
