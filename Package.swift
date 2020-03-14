f7b11a82db62bab18b777804c1ff63fc3dcfd5b9
Pierluigi Cifani <pcifani@theleftbit.com>
2020-03-14 12:34:01 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,6 @@ let package = Package(
         ),
         .library(
             name: "BSWSnapshotTest",
-            type: .dynamic, 
             targets: ["BSWSnapshotTest"]
         ),
     ],
