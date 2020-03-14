c330c0ee65b5d3afb7847e2854d55bf5c57a29e4
Pierluigi Cifani <pcifani@theleftbit.com>
2020-03-14 12:18:19 +0100

    Make the test framework dynamic

--- Package.swift
+++ Package.swift
@@ -17,6 +17,7 @@ let package = Package(
         ),
         .library(
             name: "BSWSnapshotTest",
+            type: .dynamic, 
             targets: ["BSWSnapshotTest"]
         ),
     ],
