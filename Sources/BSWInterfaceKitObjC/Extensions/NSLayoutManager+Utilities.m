d92c1b8defe13ab47a3ef684bbd5c6d819e32458
Pierluigi Cifani <pcifani@theleftbit.com>
2020-03-20 11:42:47 +0100

    Make sure the algorithm works for one line and for multiple lines

---
+++ Sources/BSWInterfaceKitObjC/Extensions/NSLayoutManager+Utilities.m
@@ -0,0 +1,29 @@
+//
+//  UIViewController+Utilities.m
+//  Created by Pierluigi Cifani on 18/04/2019.
+//
+
+#include <TargetConditionals.h>
+
+#if TARGET_OS_IOS
+
+#import "NSLayoutManager+Utilities.h"
+
+@implementation NSLayoutManager (BSWUtilities)
+
+- (NSInteger)numberOfLines {
+    NSLayoutManager *layoutManager = self;
+    NSUInteger numberOfLines, index, numberOfGlyphs =
+            [layoutManager numberOfGlyphs];
+    NSRange lineRange;
+    for (numberOfLines = 0, index = 0; index < numberOfGlyphs; numberOfLines++){
+        (void) [layoutManager lineFragmentRectForGlyphAtIndex:index
+                effectiveRange:&lineRange];
+        index = NSMaxRange(lineRange);
+    }
+    return numberOfLines;
+}
+
+@end
+
+#endif
