67e0b2a101d62871efd71fc829b0ee0679264813
Pierluigi Cifani <pcifani@theleftbit.com>
2020-06-25 12:29:24 +0200

    Create Playground as documentation and example

---
+++ Sources/BSWInterfaceKit/Documentation.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+import BSWInterfaceKit
+import Task
+
+/*:
+ Welcome to BSWInterfaceKit, the easiest way to build UIKit apps
+ 
+ Let's go over some of the basic building blocks of any new iOS app:
+ ### Bottom Action ViewControllers
+ */
+
+let bottomActionVC = ListViewController.Factory.viewController()
