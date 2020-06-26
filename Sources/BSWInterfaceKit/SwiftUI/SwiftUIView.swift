3f21311061ff406f8604d9566028d6c97bad1087
Pierluigi Cifani <pcifani@theleftbit.com>
2020-06-26 17:56:15 +0200

    Remove all SwiftUI code to start from scratch

---
+++ Sources/BSWInterfaceKit/SwiftUI/SwiftUIView.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftUIView.swift
+//  Created by Pierluigi Cifani on 26/06/2020.
+//
+
+/*
+import SwiftUI
+
+@available(iOS 13.0, *)
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+@available(iOS 13.0, *)
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
+*/
