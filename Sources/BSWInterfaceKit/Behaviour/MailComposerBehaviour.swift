9aaa24f7bd3663196d98670cef5c40dcd91249f0
Pierluigi Cifani <pcifani@theleftbit.com>
2020-12-23 11:00:47 +0000

    Add contents to Mail share feature

--- Sources/BSWInterfaceKit/Behaviour/MailComposerBehaviour.swift
+++ Sources/BSWInterfaceKit/Behaviour/MailComposerBehaviour.swift
@@ -11,14 +11,17 @@ import MessageUI
 
 final public class MessageComposerBehavior: NSObject, MFMailComposeViewControllerDelegate, MFMessageComposeViewControllerDelegate {
     public static let composer = MessageComposerBehavior()
-    
-    public func mailViewController(email: String) -> UIViewController? {
+        
+    public func mailViewController(email: String, contents: MailContents? = nil) -> UIViewController? {
         guard MFMailComposeViewController.canSendMail() else {
             return nil
         }
         let mailVC = MFMailComposeViewController()
         mailVC.mailComposeDelegate = self
         mailVC.setToRecipients([email])
+        if let contents = contents {
+            mailVC.setMessageBody(contents.body, isHTML: contents.isHTML)
+        }
         return mailVC
     }
 
@@ -39,6 +42,18 @@ final public class MessageComposerBehavior: NSObject, MFMailComposeViewControlle
     public func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
 
         controller.dismiss(animated: true, completion: nil)
+    }
+}
+
+public extension MessageComposerBehavior {
+    struct MailContents {
+        public let body: String
+        public let isHTML: Bool
+        
+        public init(body: String, isHTML: Bool) {
+            self.body = body
+            self.isHTML = isHTML
+        }
     }
 }
 
