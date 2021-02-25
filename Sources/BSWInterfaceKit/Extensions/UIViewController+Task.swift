72091b8706f7a021cccfa65ccde679c05f155218
Pierluigi Cifani <pcifani@theleftbit.com>
2021-02-25 12:56:59 +0100

    Comment the new method

--- Sources/BSWInterfaceKit/Extensions/UIViewController+Task.swift
+++ Sources/BSWInterfaceKit/Extensions/UIViewController+Task.swift
@@ -15,16 +15,26 @@ public extension UIViewController {
     typealias LoadingViewFactory = () -> (UIView)
     typealias ErrorViewFactory = (String, Error, @escaping VoidHandler) -> (UIView)
 
+    /**
+     Allows you to show a loading/error/success state in any `UIViewController`.
+     Please customize it via `loadingViewFactory` and `errorViewFactory`
+      - Parameters:
+        - taskGenerator: a closure that returns a `Task` that fetches the data
+        - animated: Indicates where the first transition to the loading phase is animated. All other transitions are animated by default.
+        - errorMessage: An optional error message to pass to `ErrorViewFactory` in case an error happens
+        - completion: A completion handler where the Success value is retrieved. Use it to configure your `viewController`.
+     */
     @discardableResult
     func fetchData<T>(taskGenerator: @escaping TaskGenerator<T>, animated: Bool = true, errorMessage: String = "error", completion: @escaping TaskCompletion<T>) -> Task<T> {
         bsw_showLoadingView(animated: animated)
         let task = taskGenerator()
         task.upon(.main) { [weak self] (result) in
-            self?.bsw_hideLoadingView()
+            guard let self = self else { return }
+            self.bsw_hideLoadingView(animated: self.defaultAnimationFlag)
             switch result {
             case .failure(let error):
                 if error.isURLCancelled { /* Don't show the error in case it's a search */ return }
-                self?.handleError(error, errorMessage: errorMessage, taskGenerator: taskGenerator, animated: animated, completion: completion)
+                self.handleError(error, errorMessage: errorMessage, taskGenerator: taskGenerator, animated: self.defaultAnimationFlag, completion: completion)
             case .success(let value):
                 completion(value)
             }
@@ -45,8 +55,20 @@ public extension UIViewController {
         showLoadingView(UIViewController.loadingViewFactory(), animated: animated)
     }
     
-    @objc func bsw_hideLoadingView() {
-        hideLoader()
+    @objc func bsw_hideLoadingView(animated: Bool) {
+        hideLoader(animated: animated)
+    }
+    
+    private var defaultAnimationFlag: Bool {
+        #if DEBUG
+        if UIApplication.shared.isRunningTests {
+            return false
+        } else {
+            return true
+        }
+        #else
+        return true
+        #endif
     }
     
     static var loadingViewFactory: LoadingViewFactory = { LoadingView() }
