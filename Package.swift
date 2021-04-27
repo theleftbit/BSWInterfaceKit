31c640734085d5f17c1e33ca04c95ce500ef8ebc
Pierluigi Cifani <pcifani@theleftbit.com>
2021-04-27 11:45:16 +0200

    Bump BSWFoundation

--- Package.swift
+++ Package.swift
@@ -13,13 +13,14 @@ let package = Package(
     products: [
         .library(
             name: "BSWInterfaceKit",
+            type: .dynamic,
             targets: ["BSWInterfaceKit", "BSWInterfaceKitObjC"]
         ),
     ],
     dependencies: [
-        .package(name: "SnapshotTesting", url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.8.1"),
-        .package(url: "https://github.com/bignerdranch/Deferred.git", from: "4.1.0"),
-        .package(url: "https://github.com/theleftbit/BSWFoundation.git", from: "4.1.4"),
+        .package(name: "SnapshotTesting", url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.8.2"),
+        .package(name: "Deferred", url: "https://github.com/theleftbit/Deferred.git", from: "4.2.0"),
+        .package(name: "BSWFoundation", url: "https://github.com/theleftbit/BSWFoundation.git", from: "4.2.0"),
         .package(url: "https://github.com/kean/Nuke.git", from: "9.5.0"),
     ],
     targets: [
