4adb52d582defb18789c14d7e2f089dcbc66d6b2
Pierluigi Cifani <pcifani@theleftbit.com>
2022-08-08 12:14:31 +0200

    Add separator view

---
+++ Sources/BSWInterfaceKit/Views/SeparatorView.swift
@@ -0,0 +1,20 @@
+#if canImport(UIKit)
+
+import UIKit
+
+public class SeparatorView: UIView {
+    public init(color: UIColor = .separator) {
+        super.init(frame: .zero)
+        let separatorView = UIView()
+        separatorView.backgroundColor = color
+        addAutolayoutSubview(separatorView)
+        separatorView.pinToSuperview()
+        NSLayoutConstraint.activate([
+            separatorView.heightAnchor.constraint(equalToConstant: 1),
+        ])
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+}
+
+#endif
