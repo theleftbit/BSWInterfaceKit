48e2959ac56095f78ede12f989fbdd6c5073c430
Pierluigi Cifani <pcifani@theleftbit.com>
2022-08-10 11:19:28 +0200

    Add .errorAlert()

---
+++ Sources/BSWInterfaceKit/SwiftUI/SwiftUI+View.swift
@@ -0,0 +1,43 @@
+
+#if canImport(UIKit)
+
+import SwiftUI
+
+public extension SwiftUI.View {
+    func asViewController() -> UIViewController {
+        return UIHostingController(rootView: self)
+    }
+}
+
+#endif
+
+
+@available(iOS 15.0, *)
+public extension View {
+    func errorAlert(error: Binding<Error?>, buttonTitle: String = "OK") -> some View {
+        let localizedAlertError = LocalizedAlertError(error: error.wrappedValue)
+        return alert(isPresented: .constant(localizedAlertError != nil), error: localizedAlertError) { _ in
+            Button(buttonTitle) {
+                error.wrappedValue = nil
+            }
+        } message: { error in
+            Text(error.recoverySuggestion ?? "")
+        }
+    }
+}
+
+@available(iOS 15.0, *)
+private struct LocalizedAlertError: LocalizedError {
+    let underlyingError: LocalizedError
+    var errorDescription: String? {
+        underlyingError.errorDescription
+    }
+    var recoverySuggestion: String? {
+        underlyingError.recoverySuggestion
+    }
+
+    init?(error: Error?) {
+        guard let localizedError = error as? LocalizedError else { return nil }
+        underlyingError = localizedError
+    }
+}
