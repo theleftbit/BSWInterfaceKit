78425b095a1730da8204d282eb7b58e09664dd7e
Marc Hidalgo <marc@theleftbit.com>
2023-01-17 14:52:21 +0100

    Remove unused code

--- Sources/BSWInterfaceKit/Cells/InfiniteLoadingCollectionViewCell.swift
+++ Sources/BSWInterfaceKit/Cells/InfiniteLoadingCollectionViewCell.swift
@@ -34,34 +34,4 @@ open class InfiniteLoadingCollectionViewCell: UICollectionViewListCell {
     }
 }
 
-public class InfiniteLoadingCollectionViewFooter: UICollectionReusableView, ViewModelReusable, CollectionViewInfiniteFooter {
-    
-    public let activityIndicator = UIActivityIndicatorView(style: .defaultStyle)
-    
-    public override init(frame: CGRect) {
-        super.init(frame: frame)
-        backgroundColor = .clear
-        addAutolayoutSubview(activityIndicator)
-        let constraints = [
-            activityIndicator.topAnchor.constraint(equalTo: topAnchor, constant: 16),
-            activityIndicator.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16),
-            activityIndicator.centerXAnchor.constraint(equalTo: centerXAnchor)
-            ]
-        constraints.forEach({ $0.priority = .init(999) })
-        NSLayoutConstraint.activate(constraints)
-    }
-    
-    required public init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    public func configureFor(viewModel: Void) {
-
-    }
-    
-    public func startAnimating() {
-        activityIndicator.startAnimating()
-    }    
-}
-
 #endif
