56243a38f0b42cab7d12281f645e37f21a5ee336
Pierluigi Cifani <pcifani@theleftbit.com>
2023-02-06 13:03:28 +0100

    Also add the API for the binding to an item

--- Sources/BSWInterfaceKit/SwiftUI/ViewModifiers/DynamicHeightSheet.swift
+++ Sources/BSWInterfaceKit/SwiftUI/ViewModifiers/DynamicHeightSheet.swift
@@ -4,10 +4,19 @@ import SwiftUI
 @available(iOS 16.0, *)
 public extension SwiftUI.View {
     func dynamicHeightSheet<Content: View>(isPresented: Binding<Bool>, onDismiss: (() -> Void)? = nil, @ViewBuilder content: @escaping () -> Content) -> some View {
-        IntrinsicHeightDetentView(
+        IntrinsicHeightDetentView_ForBool(
             hostView: self,
             contentView: content,
             isPresented: isPresented,
+            onDismiss: onDismiss
+        )
+    }
+    
+    func dynamicHeightSheet<Item: Identifiable, Content: View>(item: Binding<Item?>, onDismiss: (() -> Void)? = nil, content: @escaping (Item) -> Content) -> some View {
+        IntrinsicHeightDetentView_ForItems(
+            hostView: self,
+            contentView: content,
+            isPresented: item,
             onDismiss: onDismiss
         )
     }
@@ -22,7 +31,7 @@ private struct InnerHeightPreferenceKey: PreferenceKey {
 }
 
 @available(iOS 16.0, *)
-private struct IntrinsicHeightDetentView<Host: View, Content: View>: View {
+private struct IntrinsicHeightDetentView_ForBool<Host: View, Content: View>: View {
     
     let hostView: Host
     let contentView: () -> Content
@@ -44,5 +53,31 @@ private struct IntrinsicHeightDetentView<Host: View, Content: View>: View {
                 }
                 .presentationDetents([.height(sheetHeight)])
         }
+    }
+}
+
+@available(iOS 16.0, *)
+private struct IntrinsicHeightDetentView_ForItems<Host: View, Content: View, Item: Identifiable>: View {
+    
+    let hostView: Host
+    let contentView: (Item) -> Content
+    @Binding var isPresented: Item?
+    let onDismiss: (() -> Void)?
+    @State private var sheetHeight: CGFloat = .zero
+
+    var body: some View {
+        hostView
+            .sheet(item: $isPresented, onDismiss: onDismiss) { item in
+                contentView(item)
+                    .overlay {
+                        GeometryReader { geometry in
+                            Color.clear.preference(key: InnerHeightPreferenceKey.self, value: geometry.size.height)
+                        }
+                    }
+                    .onPreferenceChange(InnerHeightPreferenceKey.self) { newHeight in
+                        sheetHeight = newHeight
+                    }
+                    .presentationDetents([.height(sheetHeight)])
+            }
     }
 }
