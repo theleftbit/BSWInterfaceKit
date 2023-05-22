abe545627acfab87779962a5cfec9b2166da7d81
Pierluigi Cifani <pcifani@theleftbit.com>
2023-05-22 11:48:59 +0200

    Update to Nuke 12.1

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.11.0"),
-        .package(url: "https://github.com/theleftbit/BSWFoundation.git", from: "5.3.7"),
-        .package(url: "https://github.com/kean/Nuke.git", from: "11.6.4"),
+        .package(url: "https://github.com/theleftbit/BSWFoundation.git", from: "5.3.8"),
+        .package(url: "https://github.com/kean/Nuke.git", from: "12.1.0"),
     ],
     targets: [
         .target(name: "BSWInterfaceKitObjC"),
