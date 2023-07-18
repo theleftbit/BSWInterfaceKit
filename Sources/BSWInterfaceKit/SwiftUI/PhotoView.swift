9fd99a9b95e953f5ade4d7487bf5a49080e111a9
Pierluigi Cifani <pcifani@theleftbit.com>
2023-07-18 15:49:15 +0000

    Remove configuration

--- Sources/BSWInterfaceKit/SwiftUI/PhotoView.swift
+++ Sources/BSWInterfaceKit/SwiftUI/PhotoView.swift
@@ -8,24 +8,21 @@ import NukeUI
 /// Displays a `Photo` in `SwiftUI`
 public struct PhotoView: View {
     
-    public init(photo: Photo, configuration: PhotoView.Configuration = .init()) {
+    public init(photo: Photo, placeholder: Placeholder? = nil) {
         self.photo = photo
-        self.configuration = configuration
+        self.placeholder = placeholder ?? .init(shape: .rectangle, color: .init(uiColor: .systemGray2))
     }
     
     let photo: Photo
-    let configuration: Configuration
+    let placeholder: Placeholder
     @Environment(\.redactionReasons) var reasons: RedactionReasons
     
     public var body: some View {
-        Group {
-            if shouldShowPlaceholder {
-                configuration.placeholder
-            } else {
-                UIImageViewWrapper(photo: photo)
-            }
+        if shouldShowPlaceholder {
+            placeholder
+        } else {
+            UIImageViewWrapper(photo: photo)
         }
-        .apply(configuration: configuration)
     }
     
     private var shouldShowPlaceholder: Bool {
@@ -40,82 +37,38 @@ public struct PhotoView: View {
 }
 
 extension PhotoView {
-        
-    public struct Configuration {
-        let placeholder: Placeholder
-        let aspectRatio: CGFloat?
-        let width: CGFloat?
-        let height: CGFloat?
-        let contentMode: ContentMode
-        
-        public init(
-            placeholder: Placeholder = .init(shape: .rectangle),
-            aspectRatio: CGFloat? = nil,
-            width: CGFloat? = nil,
-            height: CGFloat? = nil,
-            contentMode: ContentMode = .fit) {
-                self.placeholder = placeholder
-                self.aspectRatio = aspectRatio
-                self.contentMode = contentMode
-                self.width = width
-                self.height = height
-        }
-        
-        public struct Placeholder: View {
-            
-            public init(shape: PhotoView.Configuration.Placeholder.Shape, color: Color = Color(RandomColorFactory.defaultColor)) {
-                self.shape = shape
-                self.color = color
-            }
-            
-            let shape: Shape
-            let color: Color
-            
-            public enum Shape {
-                case circle, rectangle
-            }
-            
-            public var body: some View {
-                Group {
-                    switch self.shape {
-                    case .circle:
-                        Circle()
-                    case .rectangle:
-                        Rectangle()
-                    }
-                }
-                .foregroundColor(color)
-            }
-        }
-    }
-}
-
-private extension View {
     
-    func apply(configuration: PhotoView.Configuration) -> some View {
-        /// https://sarunw.com/posts/how-to-resize-swiftui-image-and-keep-aspect-ratio/#fill
-        let view = self
-            .aspectRatio(configuration.aspectRatio, contentMode: configuration.contentMode)
-            .frame(width: configuration.width, height: configuration.height)
-        return Group {
-            if configuration.contentMode == .fill {
-                view.clipped()
-            } else {
-                view
+    public struct Placeholder: View {
+        
+        public init(shape: PhotoView.Placeholder.Shape = .rectangle, color: Color = Color(RandomColorFactory.defaultColor)) {
+            self.shape = shape
+            self.color = color
+        }
+        
+        let shape: Shape
+        let color: Color
+        
+        public enum Shape {
+            case circle, rectangle
+        }
+        
+        public var body: some View {
+            Group {
+                switch self.shape {
+                case .circle:
+                    Circle()
+                case .rectangle:
+                    Rectangle()
+                }
             }
+            .foregroundStyle(color)
         }
     }
 }
 
 private struct UIImageViewWrapper: UIViewRepresentable {
         
-    init(photo: Photo, configuration: PhotoView.Configuration = .init()) {
-        self.photo = photo
-        self.configuration = configuration
-    }
-
     let photo: Photo
-    let configuration: PhotoView.Configuration
 
     public func makeUIView(context: Context) -> UIImageView {
         let uiView = UIImageView()
