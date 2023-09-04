d1856696e4b35b4e67ff09d59f4cd7dd4f110a71
Marc Hidalgo <marc@theleftbit.com>
2023-09-04 20:34:01 +0200

    Ascendent order in InfiniteScrollingDataSource

--- Sources/BSWInterfaceKit/SwiftUI/DataSource/InfiniteScrollingDataSource.swift
+++ Sources/BSWInterfaceKit/SwiftUI/DataSource/InfiniteScrollingDataSource.swift
@@ -15,12 +15,17 @@ open class InfiniteScrollingDataSource<ListItem: Identifiable>: ObservableObject
         case canLoadMorePages(currentPage: Int)
     }
     
+    public enum Direction {
+        case ascendent
+        case descendent
+    }
+    
     public typealias ItemFetcher = (Int) async throws -> ([ListItem], Bool)
     
-    public init(currentPage: Int = 0, itemFetcher: @escaping ItemFetcher) async throws {
+    public init(currentPage: Int = 0,  direction: Direction = .descendent, itemFetcher: @escaping ItemFetcher) async throws {
         self.itemFetcher = itemFetcher
         self.state = State.canLoadMorePages(currentPage: currentPage)
-        try await loadMoreContent()
+        try await loadMoreContent(direction: direction)
     }
     
     public init(mockItems: [ListItem]) {
@@ -29,11 +34,20 @@ open class InfiniteScrollingDataSource<ListItem: Identifiable>: ObservableObject
         self.itemFetcher = { _ in return ([], false) }
     }
     
-    public func loadMoreContentIfNeeded(currentItem item: ListItem) {
-        let thresholdIndex = items.index(items.endIndex, offsetBy: -5)
-        if items.firstIndex(where: { $0.id == item.id }) == thresholdIndex {
-            Task {
-                try await loadMoreContent()
+    public func loadMoreContentIfNeeded(currentItem item: ListItem, direction: Direction = .descendent) {
+        switch direction {
+        case .descendent:
+            let thresholdIndex = items.index(items.endIndex, offsetBy: -5)
+            if items.firstIndex(where: { $0.id == item.id }) == thresholdIndex {
+                Task {
+                    try await loadMoreContent(direction: .descendent)
+                }
+            }
+        case .ascendent:
+            if items.firstIndex(where: { $0.id == item.id }) == 0 {
+                Task {
+                    try await loadMoreContent(direction: .ascendent)
+                }
             }
         }
     }
@@ -47,23 +61,35 @@ open class InfiniteScrollingDataSource<ListItem: Identifiable>: ObservableObject
     
     /// MARK: Private
     
-    @MainActor
-    private func loadMoreContent() async throws {
+    private func loadMoreContent(direction: Direction = .descendent) async throws {
         guard case .canLoadMorePages(let currentPage) = state else {
             return
         }
+
         withAnimation {
             self.state = .loading
         }
-        let (items, thereAreMorePages) = try await self.itemFetcher(currentPage)
+
+        let (newItems, thereAreMorePages) = try await self.itemFetcher(currentPage)
         let duration: Double = 0.2
         let stateAnimation = Animation.easeInOut(duration: duration)
         let itemsAnimation = Animation.easeInOut(duration: duration).delay(duration)
-        withAnimation(stateAnimation) {
-            self.state = thereAreMorePages ? .canLoadMorePages(currentPage: currentPage + 1) : .noMorePages
-        }
-        withAnimation(itemsAnimation) {
-            self.items.append(contentsOf: items)
+
+        switch direction {
+        case .descendent:
+            withAnimation(stateAnimation) {
+                self.state = thereAreMorePages ? .canLoadMorePages(currentPage: currentPage + 1) : .noMorePages
+            }
+            withAnimation(itemsAnimation) {
+                self.items.append(contentsOf: newItems)
+            }
+        case .ascendent:
+            withAnimation(stateAnimation) {
+                self.state = thereAreMorePages && currentPage - 1 >= 0 ? .canLoadMorePages(currentPage: currentPage - 1) : .noMorePages
+            }
+            withAnimation(itemsAnimation) {
+                self.items.insert(contentsOf: newItems, at: 0)
+            }
         }
     }
 }
