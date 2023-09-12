2f276a164004e2e2cb62ed9783338170c662a2bf
Pierluigi Cifani <pcifani@theleftbit.com>
2023-09-12 11:15:48 +0200

    Add a blocking UI

--- Sources/BSWInterfaceKit/SwiftUI/AsyncButton.swift
+++ Sources/BSWInterfaceKit/SwiftUI/AsyncButton.swift
@@ -41,35 +41,49 @@ public struct AsyncButton<Label: View>: View {
     @State private var state: ButtonState = .idle
     @State private var error: Swift.Error?
     @Environment(\.asyncButtonLoadingMessage) var loadingMessage
+    @Environment(\.asyncButtonLoadingStyleIsBlocking) var isBlocking
 
     public var body: some View {
         Button(
             action: {
-                Task { @MainActor in
+                Task {
                     await performAction()
                 }
             },
             label: {
-                label
-                    .opacity(state == .loading ? 0 : 1)
-                    .overlay {
-                        loadingView
-                            .opacity(state == .loading ? 1 : 0)
-                    }
+                if isBlocking {
+                    label
+                } else {
+                    label
+                        .opacity(state == .loading ? 0 : 1)
+                        .overlay {
+                            loadingView
+                                .opacity(state == .loading ? 1 : 0)
+                        }
+                }
             }
         )
-        .disabled(state == .loading)
+        .disabled((state == .loading) || (error != nil))
         .errorAlert(error: $error)
     }
     
+    @MainActor
     private func performAction() async {
+        var hudVC: UIViewController?
+        if isBlocking {
+            hudVC = presentHUDViewController()
+        }
         withAnimation {
             self.state = .loading
         }
+
         do {
             try await action()
         } catch {
             self.error = error
+        }
+        if isBlocking {
+            hudVC?.dismiss(animated: true)
         }
         withAnimation {
             self.state = .idle
@@ -83,6 +97,34 @@ public struct AsyncButton<Label: View>: View {
             if let loadingMessage {
                 Text(loadingMessage)
             }
+        }
+    }
+    
+    @ViewBuilder
+    private var hudView: some View {
+        VStack(spacing: 8) {
+            ProgressView()
+            if let loadingMessage {
+                Text(loadingMessage)
+            }
+        }
+        .padding()
+        .background(.thickMaterial)
+        .cornerRadius(3.0)
+        .frame(minWidth: 200, minHeight: 200)
+    }
+
+    private func presentHUDViewController() -> UIViewController? {
+        if let windowScene = UIApplication.shared.connectedScenes.first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene, let rootVC = windowScene.keyWindow?.rootViewController {
+            let ___hudVC = UIHostingController(rootView: hudView)
+            ___hudVC.modalPresentationStyle = .overCurrentContext
+            ___hudVC.modalTransitionStyle = .crossDissolve
+            ___hudVC.view.backgroundColor = .clear
+            ___hudVC.view.isOpaque = false
+            rootVC.present(___hudVC, animated: true)
+            return ___hudVC
+        } else {
+            return nil
         }
     }
 }
@@ -121,6 +163,14 @@ public extension View {
     func asyncButtonLoadingMessage(_ message: String) -> some View {
         self.environment(\.asyncButtonLoadingMessage, message)
     }
+
+    func asyncButtonLoadingStyle(isBlocking: Bool) -> some View {
+        self.environment(\.asyncButtonLoadingStyleIsBlocking, isBlocking)
+    }
+}
+
+private struct AsyncButtonLoadingStyleIsBlockingEnvironmentKey: EnvironmentKey {
+    static let defaultValue: Bool = false
 }
 
 private struct AsyncButtonLoadingMessageEnvironmentKey: EnvironmentKey {
@@ -131,5 +181,10 @@ private extension EnvironmentValues {
     var asyncButtonLoadingMessage: String? {
         get { self[AsyncButtonLoadingMessageEnvironmentKey.self] }
         set { self[AsyncButtonLoadingMessageEnvironmentKey.self] = newValue }
+    }
+    
+    var asyncButtonLoadingStyleIsBlocking: Bool {
+        get { self[AsyncButtonLoadingStyleIsBlockingEnvironmentKey.self] }
+        set { self[AsyncButtonLoadingStyleIsBlockingEnvironmentKey.self] = newValue }
     }
 }
