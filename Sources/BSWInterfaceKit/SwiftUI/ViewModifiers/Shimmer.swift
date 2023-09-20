7ca83b80b3c3732cc4cdeb8c29fb6486e3eaa461
Pierluigi Cifani <pcifani@theleftbit.com>
2023-09-20 11:25:19 +0200

    Fix shimmering on iOS 17

--- Sources/BSWInterfaceKit/SwiftUI/ViewModifiers/Shimmer.swift
+++ Sources/BSWInterfaceKit/SwiftUI/ViewModifiers/Shimmer.swift
@@ -3,83 +3,119 @@ import SwiftUI
 
 #if compiler(>=5.9)
 
-struct ExampleView: View {
-    var body: some View {
-        Text("Adds an animated shimmering effect to any view, typically to show that an operation is in progress")
-            .padding()
-            .redacted(reason: .placeholder)
-            .shimmering()
-    }
-}
-
 #Preview {
-    ExampleView()
+    Text("Adds an animated shimmering effect to any view, typically to show that an operation is in progress")
+        .padding()
+        .redacted(reason: .placeholder)
+        .shimmering()
 }
 #endif
 
 public extension View {
-    /// Adds an animated shimmering effect to any view, typically to show that
-    /// an operation is in progress.
+    /// Adds an animated shimmering effect to any view, typically to show that an operation is in progress.
     /// - Parameters:
     ///   - active: Convenience parameter to conditionally enable the effect. Defaults to `true`.
-    ///   - duration: The duration of a shimmer cycle in seconds. Default: `1.0`.
-    ///   - bounce: Whether to bounce (reverse) the animation back and forth. Defaults to `true`.
+    ///   - animation: A custom animation. Defaults to ``Shimmer/defaultAnimation``.
+    ///   - gradient: A custom gradient. Defaults to ``Shimmer/defaultGradient``.
+    ///   - bandSize: The size of the animated mask's "band". Defaults to 0.3 unit points, which corresponds to
+    /// 20% of the extent of the gradient.
     @ViewBuilder func shimmering(
-        active: Bool = true, duration: Double = 1.0, bounce: Bool = true
+        active: Bool = true,
+        animation: Animation = ShimmeringDefaultAnimation,
+        gradient: Gradient = ShimmeringDefaultGradient,
+        bandSize: CGFloat = 0.3
     ) -> some View {
         if active {
-            modifier(Shimmer(duration: duration, bounce: bounce))
+            modifier(Shimmer(animation: animation, gradient: gradient, bandSize: bandSize))
         } else {
             self
         }
     }
 }
 
+/// The default animation effect.
+public let ShimmeringDefaultAnimation = Animation.linear(duration: 1).delay(0.25).repeatForever(autoreverses: false)
+
+// A default gradient for the animated mask.
+public let ShimmeringDefaultGradient = Gradient(colors: [
+    .black.opacity(0.3), // translucent
+    .black, // opaque
+    .black.opacity(0.3) // translucent
+])
+
 /// A view modifier that applies an animated "shimmer" to any view, typically to show that
 /// an operation is in progress.
 private struct Shimmer: ViewModifier {
-    @State private var phase: CGFloat = 0
-    var duration = 1.5
-    var bounce = false
+    private let animation: Animation
+    private let gradient: Gradient
+    private let min, max: CGFloat
+    @State private var isInitialState = true
+    @Environment(\.layoutDirection) private var layoutDirection
+
+    /// Initializes his modifier with a custom animation,
+    /// - Parameters:
+    ///   - animation: A custom animation. Defaults to ``Shimmer/defaultAnimation``.
+    ///   - gradient: A custom gradient. Defaults to ``Shimmer/defaultGradient``.
+    ///   - bandSize: The size of the animated mask's "band". Defaults to 0.3 unit points, which corresponds to
+    /// 30% of the extent of the gradient.
+    public init(
+        animation: Animation,
+        gradient: Gradient,
+        bandSize: CGFloat = 0.3
+    ) {
+        self.animation = animation
+        self.gradient = gradient
+        // Calculate unit point dimensions beyond the gradient's edges by the band size
+        self.min = 0 - bandSize
+        self.max = 1 + bandSize
+    }
+
+    /*
+     Calculating the gradient's animated start and end unit points:
+     min,min
+        \
+         ┌───────┐         ┌───────┐
+         │0,0    │ Animate │       │  "forward" gradient
+     LTR │       │ ───────►│    1,1│  / // /
+         └───────┘         └───────┘
+                                    \
+                                  max,max
+                max,min
+                  /
+         ┌───────┐         ┌───────┐
+         │    1,0│ Animate │       │  "backward" gradient
+     RTL │       │ ───────►│0,1    │  \ \\ \
+         └───────┘         └───────┘
+                          /
+                       min,max
+     */
+
+    /// The start unit point of our gradient, adjusting for layout direction.
+    var startPoint: UnitPoint {
+        if layoutDirection == .rightToLeft {
+            return isInitialState ? UnitPoint(x: max, y: min) : UnitPoint(x: 0, y: 1)
+        } else {
+            return isInitialState ? UnitPoint(x: min, y: min) : UnitPoint(x: 1, y: 1)
+        }
+    }
+
+    /// The end unit point of our gradient, adjusting for layout direction.
+    var endPoint: UnitPoint {
+        if layoutDirection == .rightToLeft {
+            return isInitialState ? UnitPoint(x: 1, y: 0) : UnitPoint(x: min, y: max)
+        } else {
+            return isInitialState ? UnitPoint(x: 0, y: 0) : UnitPoint(x: max, y: max)
+        }
+    }
 
     public func body(content: Content) -> some View {
         content
-            .modifier(AnimatedMask(phase: phase).animation(
-                Animation.linear(duration: duration)
-                    .repeatForever(autoreverses: bounce)
-            ))
-            .onAppear { phase = 0.8 }
-    }
-
-    /// An animatable modifier to interpolate between `phase` values.
-    struct AnimatedMask: AnimatableModifier {
-        var phase: CGFloat = 0
-
-        var animatableData: CGFloat {
-            get { phase }
-            set { phase = newValue }
-        }
-
-        func body(content: Content) -> some View {
-            content
-                .mask(GradientMask(phase: phase).scaleEffect(3))
-        }
-    }
-
-    /// A slanted, animatable gradient between transparent and opaque to use as mask.
-    /// The `phase` parameter shifts the gradient, moving the opaque band.
-    struct GradientMask: View {
-        let phase: CGFloat
-        let centerColor = Color.black
-        let edgeColor = Color.black.opacity(0.3)
-
-        var body: some View {
-            LinearGradient(gradient:
-                Gradient(stops: [
-                    .init(color: edgeColor, location: phase),
-                    .init(color: centerColor, location: phase + 0.1),
-                    .init(color: edgeColor, location: phase + 0.2)
-                ]), startPoint: .topLeading, endPoint: .bottomTrailing)
-        }
+            .mask(LinearGradient(gradient: gradient, startPoint: startPoint, endPoint: endPoint))
+            .onAppear {
+                withAnimation(animation) {
+                    isInitialState = false
+                }
+            }
     }
 }
+
