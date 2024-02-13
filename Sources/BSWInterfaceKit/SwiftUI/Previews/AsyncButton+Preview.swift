61c53b10387ccd8690428cb9b1016aeb9e9a8fd3
Pierluigi Cifani <pcifani@theleftbit.com>
2024-02-13 16:07:06 +0100

    Improve Preview

--- Sources/BSWInterfaceKit/SwiftUI/Previews/AsyncButton+Preview.swift
+++ Sources/BSWInterfaceKit/SwiftUI/Previews/AsyncButton+Preview.swift
@@ -1,24 +1,22 @@
 import SwiftUI
 
-@available(iOS 16.0, *)
-struct AsyncButton_Previews: PreviewProvider {
-    static var previews: some View {
-        AsyncButton {
-            try await Task.sleep(for: .seconds(1.5))
-            struct SomeError: Swift.Error {}
-        } label: {
-            Label(
-                title: { Text("Touch Me") },
-                icon: { Image(systemName: "42.circle") }
-            )
-            .frame(maxWidth: .infinity)
-        }
-        .buttonStyle(.borderedProminent)
-        .frame(width: 320)
-        .font(.headline)
-        .asyncButtonLoadingConfiguration(
-            message: "Loading...",
-            style: .blocking(font: .headline, dimsBackground: true)
-        )
-    }
+@available(iOS 17, *)
+#Preview {
+  AsyncButton {
+      try await Task.sleep(for: .seconds(1.5))
+      struct SomeError: Swift.Error {}
+  } label: {
+      Label(
+          title: { Text("Touch Me") },
+          icon: { Image(systemName: "42.circle") }
+      )
+      .frame(maxWidth: .infinity)
+  }
+  .buttonStyle(.borderedProminent)
+  .padding()
+  .font(.headline)
+  .asyncButtonLoadingConfiguration(
+      message: "Loading...",
+      style: .nonblocking
+  )
 }
