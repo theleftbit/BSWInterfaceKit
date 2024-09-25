aabb73f735854dcc9f0c1ec48869b589e5619bd5
Pierluigi Cifani <pcifani@theleftbit.com>
2024-09-25 18:09:08 +0200

    Improve the API for AsyncView

--- Sources/BSWInterfaceKit/SwiftUI/Protocols/PlaceholderDataProvider.swift
+++ Sources/BSWInterfaceKit/SwiftUI/Protocols/PlaceholderDataProvider.swift
@@ -1,8 +1,8 @@
+import SwiftUI
 
 /// A protocol that defines types that return placeholder data to be used for Previews or loading states.
-public protocol PlaceholderDataProvider {
-    associatedtype Data
+public protocol PlaceholderDataProvider: SwiftUI.View {
+    associatedtype PlaceholderData
     
-    @MainActor
-    static func generatePlaceholderData() -> Data
+    static func generatePlaceholderData() -> PlaceholderData
 }
