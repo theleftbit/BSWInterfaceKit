217d5b157c4aa20ff836e4f54f38a00b6be2f0f0
michele-theleftbit <michele@theleftbit.com>
2025-02-03 12:39:45 +0100

    Improve error message

--- Sources/BSWInterfaceKit/SwiftUI/Extensions/SwiftUI+View.swift
+++ Sources/BSWInterfaceKit/SwiftUI/Extensions/SwiftUI+View.swift
@@ -47,14 +47,29 @@ private struct ErrorAwareView: ViewModifier {
                 presenting: errorBinding.wrappedValue,
                 actions: { error in
                     Button("dismiss".localized, action: { })
-                }, message: {
-                    if let localizedError = $0 as? LocalizedError,
-                       let failureReason = localizedError.errorDescription {
-                        Text(failureReason)
-                    } else {
+                }, message: { err in
+                    if ModuleDependencies.enhancedErrorLogs {
                         Text("Something went wrong")
+                    } else {
+                        if let localizedError = err as? LocalizedError,
+                           let failureReason = localizedError.errorDescription {
+                            if !ModuleDependencies.enhancedErrorLogs, let range = failureReason.range(of: #"\"message\":\"([^\"]+)\""#, options: .regularExpression) {
+                                let extractedMessage = String(failureReason[range])
+                                    .replacingOccurrences(of: "\"message\":\"", with: "")
+                                    .replacingOccurrences(of: "\"", with: "")
+                                Text(extractedMessage)
+                            } else {
+                                Text(failureReason)
+                            }
+                        } else {
+                            Text("Something went wrong")
+                        }
                     }
                 }
             )
     }
+}
+
+public enum ModuleDependencies {
+    public static nonisolated(unsafe) var enhancedErrorLogs: Bool = true
 }
