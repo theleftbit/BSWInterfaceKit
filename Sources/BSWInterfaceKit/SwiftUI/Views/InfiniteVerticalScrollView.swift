3ca5f29a4a5046289f675185775ac3313dc56182
Pierluigi Cifani <pcifani@theleftbit.com>
2025-02-12 18:04:09 +0100

    Only use one modifier

--- Sources/BSWInterfaceKit/SwiftUI/Views/InfiniteVerticalScrollView.swift
+++ Sources/BSWInterfaceKit/SwiftUI/Views/InfiniteVerticalScrollView.swift
@@ -139,7 +139,7 @@ public struct InfiniteVerticalScrollView<Item: Identifiable & Sendable, ItemView
     private var phase: Phase = .idle
     
     @State
-    private var scrollPosition = ScrollPosition()
+    private var scrollPosition = ScrollPosition(idType: Item.ID.self)
     
     @State
     private var isScrolling = false
@@ -173,7 +173,6 @@ public struct InfiniteVerticalScrollView<Item: Identifiable & Sendable, ItemView
             LazyVStack(alignment: alignment, spacing: spacing, pinnedViews: pinnedViews) {
                 ForEach(items) { item in
                     itemViewBuilder(item)
-                        .id(item.id)
                 }
             }
             .scrollTargetLayout()
@@ -184,7 +183,6 @@ public struct InfiniteVerticalScrollView<Item: Identifiable & Sendable, ItemView
                     .padding()
             }
         }
-        .defaultScrollAnchor((direction == .downwards) ? .top : .bottom)
         .scrollPosition($scrollPosition, anchor: (direction == .downwards) ? .bottom : .top)
         .onScrollTargetVisibilityChange(idType: Item.ID.self, threshold: 0.9) { ids in
             if redactionReasons.contains(.placeholder) { return }
@@ -198,6 +196,16 @@ public struct InfiniteVerticalScrollView<Item: Identifiable & Sendable, ItemView
             if let anchorItemID, newValue.contains(anchorItemID), phase == .idle, isScrolling {
                 let newPhase = Phase.paging(fromItem: anchorItemID)
                 self.phase = newPhase
+            }
+        }
+        .onAppear {
+            /// The first time we're shown on screen, scroll to where we should
+            /// We had a _lot_ of weird issues because we were using these two modifiers:
+            /// https://developer.apple.com/documentation/swiftui/view/defaultscrollanchor(_:)
+            /// https://developer.apple.com/documentation/swiftui/view/scrollposition(_:anchor:)
+            /// Using only the iOS 18 version and this workaround seems to alleviate the issues
+            if visibleItemIDs.isEmpty {
+                scrollPosition.scrollTo(edge: (direction == .downwards) ? .top : .bottom)
             }
         }
         .task(id: phase) {
@@ -217,7 +225,7 @@ public struct InfiniteVerticalScrollView<Item: Identifiable & Sendable, ItemView
                     case .upwards:
                         self.items.insert(contentsOf: newItems, at: 0)
                     }
-                    self.scrollPosition.scrollTo(id: itemID)
+                    self.scrollPosition.scrollTo(id: itemID, anchor: (direction == .downwards) ? .bottom : .top)
                 }
             } catch {
                 self.phase = .idle
