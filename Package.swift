b07fe8ad683fd140d79af92c0c1768255c79de3d
Pierluigi Cifani <pcifani@theleftbit.com>
2025-04-29 15:16:57 +0200

    Start skip

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.17.4"),
-        .package(url: "https://github.com/theleftbit/BSWFoundation.git", from: "7.0.0"),
+        .package(url: "https://github.com/theleftbit/BSWFoundation.git", branch: "bump-skip"),
         .package(url: "https://github.com/kean/Nuke.git", from: "12.8.0"),
     ],
     targets: [
