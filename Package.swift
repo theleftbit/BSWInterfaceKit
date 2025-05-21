df42437a84108af755299d76b19cbb0717086dd6
Marc Hidalgo <marc@theleftbit.com>
2025-05-21 11:25:39 +0200

    Use custom skip

--- Package.swift
+++ Package.swift
@@ -30,7 +30,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.18.3"),
         .package(url: "https://github.com/theleftbit/BSWFoundation.git", from: "7.1.0"),
-        .package(url: "https://github.com/kean/Nuke.git", from: "12.8.0"),
+        .package(url: "https://github.com/MarcHidalgo5/Nuke.git", branch: "only-ios"),
         .package(url: "https://source.skip.tools/skip.git", from: "1.5.14"),
         .package(url: "https://source.skip.tools/skip-fuse-ui.git", from: "0.14.0"),
     ],
