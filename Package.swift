3b1a5e786bee832e27e2fdaf4da842c8d015320b
Pierluigi Cifani <pcifani@theleftbit.com>
2025-08-26 14:49:06 +0000

    Remove this

--- Package.swift
+++ Package.swift
@@ -74,7 +74,10 @@ let package = Package(
         ),
         .testTarget(
             name: "BSWInterfaceKitTests",
-            dependencies: ["BSWInterfaceKit", .product(name: "SnapshotTesting", package: "swift-snapshot-testing")],
+            dependencies: [
+                "BSWInterfaceKit",
+                .product(name: "SnapshotTesting", package: "swift-snapshot-testing")
+            ],
             exclude: ["Suite/__Snapshots__/"]
         ),
     ]
