cfe2f86a93e69676d8bb8c478ad9a0892731f511
Marc Hidalgo <marc@theleftbit.com>
2025-09-03 10:56:20 +0200

    Revert "Make this filePrivate"
    This reverts commit 76befa0ae6bb1bcaa0bdefa37e0168334d94cf83.

--- Sources/BSWInterfaceKit/SwiftUI/ViewModifiers/IntrinsicHeightSheet.swift
+++ Sources/BSWInterfaceKit/SwiftUI/ViewModifiers/IntrinsicHeightSheet.swift
@@ -56,7 +56,7 @@ public extension View {
     }
 }
 
-fileprivate struct IntrinsicHeightDetentView_ForBool<Host: View, Content: View>: View {
+struct IntrinsicHeightDetentView_ForBool<Host: View, Content: View>: View {
     
     let hostView: Host
     let contentView: () -> Content
@@ -82,7 +82,7 @@ fileprivate struct IntrinsicHeightDetentView_ForBool<Host: View, Content: View>:
     }
 }
 
-fileprivate struct IntrinsicHeightDetentView_ForItems<Host: View, Content: View, Item: Identifiable>: View {
+struct IntrinsicHeightDetentView_ForItems<Host: View, Content: View, Item: Identifiable>: View {
     
     let hostView: Host
     let contentView: (Item) -> Content
