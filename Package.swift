85a7b36bac53a6bf832b51905ce7a40de0ca5bef
Pierluigi Cifani <pcifani@theleftbit.com>
2025-10-03 12:18:39 +0200

    Bump this

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let applePlatforms = TargetDependencyCondition.when(
 )
 
 var packageDependencies: [Package.Dependency] = [
-    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.18.3"),
+    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.18.7"),
     .package(url: "https://github.com/theleftbit/BSWFoundation.git", from: "7.2.4"),
     .package(url: "https://github.com/kean/Nuke.git", from: "12.8.0"),
 ]
