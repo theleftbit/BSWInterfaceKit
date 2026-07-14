e0a0128b38517b26d677ea0e087e8604dff52e3c
michele-theleftbit <michele@theleftbit.com>
2026-07-14 11:14:14 +0200

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:6.2
+// swift-tools-version:6.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -18,15 +18,15 @@ let applePlatforms = TargetDependencyCondition.when(
 )
 
 var packageDependencies: [Package.Dependency] = [
-    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.18.7"),
-    .package(url: "https://github.com/theleftbit/BSWFoundation.git", from: "7.4.0"),
+    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.19.3"),
+    .package(url: "https://github.com/theleftbit/BSWFoundation.git", from: "8.0.0"),
     .package(url: "https://github.com/kean/Nuke.git", from: "12.8.0"),
 ]
 
 if skipIsEnabled {
     packageDependencies.append(contentsOf: [
-        .package(url: "https://source.skip.tools/skip.git", from: "1.8.0"),
-        .package(url: "https://source.skip.tools/skip-fuse-ui.git", from: "1.14.2"),
+        .package(url: "https://source.skip.tools/skip.git", from: "1.9.4"),
+        .package(url: "https://source.skip.tools/skip-fuse-ui.git", from: "1.17.3"),
     ])
 }
 
